79b21dae94c7f422aeb8cd4c02b768f9b9c21d84
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2023-06-13 23:58:11 +0700

    treewide: Decommonized to lemonade
    * With latest lemonade repo at LineageOS/android_device_oneplus_lemonade@a2bfb5a
    
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter lemonade lemonadep lunaa martini,$(TARGET_DEVICE)),)
+ifeq ($(TARGET_DEVICE),lemonade)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
