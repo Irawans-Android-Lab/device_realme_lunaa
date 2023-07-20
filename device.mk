0c918350e973528df7d994df362d3cbe44a9a6a1
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2023-07-20 00:29:37 +0000

    lunaa: Bringup PixelExperience
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -321,7 +321,7 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-pe
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
