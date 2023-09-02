a15fc08e9246cf668226bc1384fb0039a8995c4f
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2023-09-02 10:16:18 +0700

    Revert "lunaa: Don't build common multihal sensors"
    This reverts commit a630a07aa6876af95d160603b92627bbe6f97bc8.
    
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -383,8 +383,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    als_correction_service.lunaa \
-    android.hardware.sensors@2.0-service.multihal.lunaa \
+    android.hardware.sensors@2.0-service.multihal \
     libsensorndkbridge \
     sensors.oplus
 
@@ -406,6 +405,11 @@ PRODUCT_COPY_FILES += \
 
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 30
+
+# Sensors
+PRODUCT_PACKAGES += \
+    als_correction_service.lunaa \
+    android.hardware.sensors@2.0-service.multihal.lunaa
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
